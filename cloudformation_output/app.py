6cabe3b2e996ef8232e8ddbef2ed3cc68268a765
faithtosin.olapade <f.olapade@reply.it>
2021-05-04 17:05:00 +0200

    repo clean up

--- cloudformation_output/app.py
+++ cloudformation_output/app.py
@@ -17,6 +17,7 @@ def lambda_handler(event, context):
     response_list_stack = cloudformation.list_stacks(StackStatusFilter=['CREATE_COMPLETE','UPDATE_COMPLETE'])
     output_stack = response_list_stack["StackSummaries"]
     
+    stacks_output_list = [] 
     #Loop through each stack
     for i in output_stack:
         stack_name = i["StackName"]
@@ -30,9 +31,15 @@ def lambda_handler(event, context):
             
             InputEndpoints = []
             ChannelID=""
+            CloudFrontHlsEndpoint=""
+            MediaPackageHlsEndpoint=""
             for output in outputs:
                 if output["OutputKey"] == "MediaLiveChannelId":
-                    ChannelID= "ChannelID: "+ output["OutputValue"]
+                    ChannelID= output["OutputValue"]
+                elif output["OutputKey"] == "CloudFrontHlsEndpoint":
+                    CloudFrontHlsEndpoint = output["OutputValue"]
+                elif output["OutputKey"] == "MediaPackageHlsEndpoint":
+                    MediaPackageHlsEndpoint = output["OutputValue"]
                 elif output["OutputKey"] == "MediaLivePrimaryEndpoint":
                     MediaLivePrimaryEndpoint = output["OutputValue"]
                     InputEndpoints.append(MediaLivePrimaryEndpoint)
@@ -40,17 +47,21 @@ def lambda_handler(event, context):
                     MediaLiveSecondaryEndpoint = output["OutputValue"]
                     InputEndpoints.append(MediaLiveSecondaryEndpoint)
                 
-            if InputEndpoints and ChannelID != "" :
-                yaml_file = { "Label" : stack_name,
-                        "ChannelID":ChannelID ,
-                           "InputEndpoints": InputEndpoints
-                    }
+            if  InputEndpoints and ChannelID != "" :
+                yaml_file = { "StackName" : stack_name, 
+                "ChannelID":ChannelID, 
+                "InputEndpoints": InputEndpoints,  
+                "CloudFrontHlsEndpoint": CloudFrontHlsEndpoint, 
+                "MediaPackageHlsEndpoint": MediaPackageHlsEndpoint,
+                }
                 
+                stacks_output_list.append(yaml_file)
                 print(yaml_file)    
                     
-                
-
+               
+    
     return {
         "statusCode": 200,
-        "body": json.dumps(yaml_file),
+        "body": json.dumps(stacks_output_list),
+        #"body": json.dumps(yaml_file),
     }
